12cd7d0fdf8aecd59e60437a8dfddc9cc87a6394
Zm86m <164696000+Zm86m@users.noreply.github.com>
2024-09-14 10:58:28 -0400

    Update eaglercraft_opts.js

--- web/eaglercraft_opts.js
+++ web/eaglercraft_opts.js
@@ -4,7 +4,7 @@ window.eaglercraftXOpts = () => ({
   // assetsURI: "custom.epk",
 	servers: [
 		{ addr: "ws" + "s://zentic.cc/", name: "Zentic" },
-		{ addr: "ws" + "s://pps.p", name: "Osiah" }
+		{ addr: "ws" + "s://potential-goldfish-pj79v5j74rrw27w7r-8081.app.github.dev/", name: "Osiah" }
 	]
 });
 // document.title = "My Original Server Name:tm:";
